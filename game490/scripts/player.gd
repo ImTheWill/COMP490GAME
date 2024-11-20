6e05005697e22fbfb4c51150d588a68873ccae7d
HONGYANGGAN <kam667755@gmail.com>
2024-11-19 23:23:22 -0800

    Adjust background size to correct the previous version

--- game490/scripts/player.gd
+++ game490/scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const RESET_POSITION = Vector2(0,0) #Reset position for the player
+const OUT_OF_BOUNDS_Y = 500 #Y-coordinate below which the player resets
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var camera: Camera2D = $Camera2D
@@ -11,6 +13,11 @@ func _ready():
 	camera.make_current()
 	
 func _physics_process(delta: float) -> void:
+	
+	#Check if the player falls out of bounds
+	if global_position.y > OUT_OF_BOUNDS_Y:
+		reset_player_position()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -47,10 +54,17 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.play("jump")
 	
 	
-	
+	# Horizontal movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	
+	#Reset the player's position when out of bounds
+func reset_player_position():
+		global_position = RESET_POSITION
+		velocity = Vector2(0,0) #Stop all movement
+		print ("Player resedt to:", RESET_POSITION)
