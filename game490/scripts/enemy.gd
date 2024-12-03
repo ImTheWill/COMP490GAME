c047fadefc211097257d11aec1fe59392e52f08e
ImTheWill <128171764+ImTheWill@users.noreply.github.com>
2024-12-02 19:06:15 -0800

    added enemy tracking on tile

--- game490/scripts/enemy.gd
+++ game490/scripts/enemy.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 var HEALTH = 100
-var speed = 25
+var speed = 60
 var player_chase = false
 var player = null
-var facing_right = true
+var facing_right = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var ray_cast= $RayCast2D
+
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -26,11 +28,23 @@ func _physics_process(delta):
 		$AnimatedSprite2D.play("run")
 	else:
 		$AnimatedSprite2D.play("idle")
+		
+	if !ray_cast.is_colliding() && is_on_floor():
+		flip()
+	
 	move_and_slide()
 	
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
+
+func flip():
+	facing_right != facing_right
+	scale.x = abs(scale.x)*-1
+	if(facing_right):
+		speed = speed
+	else:
+		speed = -1*speed
 
 func _on_detection_area_body_entered(body):
 	if body is bullet:
